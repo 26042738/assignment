e11d7ad7dcd7094fe0e5fe0992a74dd7f830a846
26042738 <26042738@mvula.univen.ac.za>
2026-08-26 10:58:20 +0200

    First commit

---
+++ Main.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.main;
+
+/**
+ *
+ * @author vusim
+ */
+import java.util.Scanner;
+public class Main{
+    public static void main (String[]args){
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("BRIGHT FUTURE TECHNOLOGIES APPLICATION");
+        System.out.println("**************************************");
+        while (true) {
+        System.out.println("");
+        System.out.print("Enter (1) to launch menu or any other key to exit ");
+        String value = s.nextLine();
+        System.out.println("");
+        if("1".equals(value.trim())){
+            int choice = Products.DisplayMenu();
+            switch(choice){
+                case 1: Products.CaptureProduct();
+                    break;
+                case 2: Products.SearchProduct();
+                    break;
+                case 3: Products.UpdateProduct();
+                    break;
+                case 4: Products.DeleteProduct();
+                    break;
+                case 5: Products.PrintProductReport();
+                    break;
+                case 6: Products.ExitApplication();
+                    break;
+            }
+        }
+        else{  
+            Products.ExitApplication();
+        } 
+            
+        }
+    
+    }
+    
+}
